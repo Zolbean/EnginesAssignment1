b29a3f066e26c5df8746642278a45985b4e90b66
Sean Binnie <100623774@oncampus.local>
2018-09-27 16:02:29 -0400

    MoreStuff
    Good Horse Movement, Player thrown in

---
+++ Engines Assignment 1/Assets/Scripts And DLLs/RandomMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomMovement : MonoBehaviour {
+    Vector3 rDir;
+    float cdTime;
+    public int speed = 10;
+
+    // Use this for initialization
+    void Start () {
+        rDir = new Vector3(Random.Range(-100, 101), 0, Random.Range(-100, 100));
+        rDir = rDir / 100;
+        cdTime = Random.Range(1, 5);
+        print(cdTime);
+        print(rDir);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        cdTime = cdTime - Time.deltaTime;
+        if (cdTime <= 0)
+        {
+            rDir = new Vector3(Random.Range(-100, 101), 0, Random.Range(-100, 100));
+            rDir = rDir / 100;
+            cdTime = Random.Range(1, 5);
+            print(cdTime);
+            print(rDir);
+        }
+        GetComponent<Rigidbody>().velocity = rDir * speed;
+
+        gameObject.transform.rotation = Quaternion.LookRotation(new Vector3(rDir.x, rDir.y, rDir.z)*-1);
+    }
+}
